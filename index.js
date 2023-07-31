c04dcaf88414f42818c43b1724b588fb204db90d
J.K.Sutradhor <jksutradhor@gmail.com>
2023-07-31 17:56:05 +0600

    Middleware added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const todoHandler = require("./routeHandler/todoHandler")
 const attendanceHandler = require("./routeHandler/attendanceHandler")
 const employeeHandler = require("./routeHandler/employeeHandler")
 const userHandler = require("./routeHandler/userHandler")
+const checkLogin = require("./middlewares/checkLogin");
 
 const app = express()
 dotenv.config()
@@ -25,9 +26,9 @@ mongoose.connect(process.env.MONGO_URI)
     .then(()=> console.log("Connection Successful with MongoDB"))
     .catch((err)=> console.log("ERR : ",err))
 
-app.use("/", cors(corsOptions), employeeHandler)
-app.use("/emp", cors(corsOptions), attendanceHandler)
-app.use("/user", cors(corsOptions), userHandler)
+app.use("/", cors(corsOptions), checkLogin, employeeHandler)
+app.use("/emp", cors(corsOptions), checkLogin, attendanceHandler)
+app.use("/user", cors(corsOptions), checkLogin, userHandler)
 
 app.listen(process.env.PORT || app.config.port, ()=> {
     console.log(`App listening at port ${process.env.PORT || app.config.port}`)
